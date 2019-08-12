f522fa036241df5cf1dfdafecd97b59de37e185b
yey <33614783+yeyun11@users.noreply.github.com>
2019-08-12 16:59:32 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,9 @@
 import os
 import argparse
-
+import time
 import torch
 from torchvision import transforms
 from torchvision.datasets import cifar
-
 from networks import resnet18, vgg11, vgg11s, densenet63
 from netslim import load_pruned_model
 
@@ -18,9 +17,9 @@ archs = {
 }
 
 # Training settings
-parser = argparse.ArgumentParser(description='PyTorch Cifar-100 Example for Loading Pruned Model')
+parser = argparse.ArgumentParser(description='PyTorch Cifar-100 Example for Test Pruned Model')
 parser.add_argument('--test-batch-size', type=int, default=50, metavar='N',
-                    help='input batch size for testing (default: 1000)')
+                    help='input batch size for testing (default: 50)')
 parser.add_argument('--resume-path', default='',
                     help='path to a trained model weight')
 parser.add_argument('--arch', default='resnet18',
@@ -50,13 +49,17 @@ model = load_pruned_model(model, pruned_weights).to(device)
 model.eval()
 correct = 0
 with torch.no_grad():
+    t_start = time.time()
     for data, target in test_loader:
         if args.cuda:
             data, target = data.to(device), target.to(device)
         output = model(data)
         pred = output.max(1, keepdim=True)[1]   # get the index of the max log-probability
         correct += pred.eq(target.data.view_as(pred)).cpu().sum()
+    t_all = time.time() - t_start
 
 accuracy = 100. * float(correct) / float(len(test_loader.dataset))
 print("Accuracy: {}/{} ({:.2f}%)\n".format(correct, len(test_loader.dataset), accuracy))
-    
+print("Total time: {:.2f} s".format(t_all))
+#if args.test_batch_size == 1:
+#    print("Estimated FPS: {:.2f}".format(1/(t_all/len(test_loader))))
